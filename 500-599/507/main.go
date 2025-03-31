45825f62c839ff55e06769875e053ba76d0eeebb
Alan Kochiev <117481481+tarmalonchik@users.noreply.github.com>
2025-03-31 15:14:58 +0400

    507

---
+++ 500-599/507/main.go
@@ -0,0 +1,17 @@
+package main
+
+func checkPerfectNumber(num int) bool {
+	sum := 0
+	for i := 1; i <= num/2; i++ {
+		if num%i == 0 {
+			sum += i
+			if sum > num {
+				return false
+			}
+		}
+	}
+	if sum == num {
+		return true
+	}
+	return false
+}
