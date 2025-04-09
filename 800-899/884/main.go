a187a75b17c5aea1ff6af851aa7afe8f8032b87e
Alan Kochiev <117481481+tarmalonchik@users.noreply.github.com>
2025-04-09 13:27:53 +0400

    884

--- 800-899/884/main.go
+++ 800-899/884/main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"strings"
+)
 
+func uncommonFromSentences(s1 string, s2 string) []string {
+	items := strings.Split(s1+" "+s2, " ")
+	mpSingle := make(map[string]interface{})
+	mpMultiple := make(map[string]interface{})
+
+	for i := range items {
+		if _, ok := mpMultiple[items[i]]; ok {
+			continue
+		}
+		if _, ok := mpSingle[items[i]]; ok {
+			delete(mpSingle, items[i])
+			mpMultiple[items[i]] = items[i]
+			continue
+		}
+		mpSingle[items[i]] = nil
+	}
+	out := make([]string, 0, len(mpSingle))
+	for key := range mpSingle {
+		out = append(out, key)
+	}
+	return out
 }
