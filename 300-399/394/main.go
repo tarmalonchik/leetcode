1467d5c814b73a124dcd2092bad21225e0068d6f
Alan Kochiev <117481481+tarmalonchik@users.noreply.github.com>
2025-04-28 15:13:06 +0400

    394

--- 300-399/394/main.go
+++ 300-399/394/main.go
@@ -1,69 +1,89 @@
 package main
 
-import (
-	"fmt"
-)
-
-func main() {
-	fmt.Println(decodeString("3[a2[c]]"))
-}
-
 func decodeString(s string) string {
-	out := make([]byte, 0)
-	out, _ = recursive(s, out, 0)
-	return string(out)
+	return string(decodeByte([]byte(s)))
 }
 
-func recursive(s string, out []byte, skip int) ([]byte, int) {
-	num := 0
+func decodeByte(s []byte) []byte {
+	out := make([]byte, 0)
+	pos1 := 0
+	for {
+		if pos1 >= len(s) {
+			break
+		}
 
-	for i := 0; i < len(s); i++ {
-		dig := toDig(s[i])
-		if dig != -1 {
-			num *= 10
-			num += dig
+		if isSymbol(s[pos1]) {
+			out = append(out, s[pos1])
+			pos1++
 			continue
 		}
+
+		newPos1, newPos2, num := findBraces(s[pos1:])
+		if newPos1 == -1 {
+			out = append(out, s[pos1:]...)
+			break
+		}
+		newPos1 += pos1
+		newPos2 += pos1
+
+		cp := makeCopy(decodeByte(s[newPos1+1:newPos2]), num)
+		out = append(out, cp...)
+		pos1 = newPos2 + 1
+	}
+	return out
+}
+
+func makeCopy(s []byte, i int) (out []byte) {
+	if i <= 1 {
+		return s
+	}
+	out = make([]byte, 0, len(s)*i)
+	for range i {
+		out = append(out, s...)
+	}
+	return out
+}
+
+func getNum(in byte) int {
+	if in < 48 || in > 57 {
+		return -1
+	}
+	return int(in - 48)
+}
+
+func isSymbol(in byte) bool {
+	if in >= 97 && in <= 122 {
+		return true
+	}
+	return false
+}
+
+func findBraces(s []byte) (pos1, pos2, num int) {
+	count := 0
+	for i := range s {
 		if s[i] == '[' {
-			skip++
+			if count == 0 {
+				pos1 = i
+			}
+			count++
+			continue
+		}
+		if count == 0 {
+			n := getNum(s[i])
+			if n >= 0 {
+				if n == 0 {
+				}
+				num *= 10
+				num += n
+			}
 			continue
 		}
 		if s[i] == ']' {
-			if skip == 0 {
-				return out, i
-			}
-			skip--
-			continue
-		}
-
-		startPos := 0
-		for range num {
-			startPos = i
-			for {
-				if toDig(s[startPos]) != -1 {
-					offset := 0
-					out, offset = recursive(s[startPos:], out, 0)
-					if offset == -1 {
-						break
-					}
-					startPos += offset
-				}
-				if s[startPos] == ']' {
-					break
-				}
-				out = append(out, s[startPos])
-				startPos++
+			count--
+			if count == 0 {
+				return pos1, i, num
 			}
 		}
-		i = startPos
-		num = 0
 	}
-	return out, -1
-}
-
-func toDig(in byte) int {
-	if in >= 48 && in <= 57 {
-		return int(in) - 48
-	}
-	return -1
+	return -1, -1, -1
 }
